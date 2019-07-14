1c918c449004cdc3d8edd8a0719f73a17405de0a
jadenASC6 <jadenthakur14@gmail.com>
2019-07-13 23:47:28 -0400

    impPythCalc

---
+++ impPythCalc.js
@@ -0,0 +1,29 @@
+function deriv(a,b) {
+    let pow = b;
+    let coeff = a;
+    let deriva = `${coeff*pow}x^${pow - 1}`;
+        return deriva;/*else if (process.argv[2] && !process.argv[3]) {
+        return 'You need to input the exponent!';
+    } else if (!process.argv[2]) {
+        return 'There\'s nothing to derive!';
+    }*/
+}
+
+function hypo() {
+    return  Math.sqrt(Math.pow(process.argv[3], 2) + Math.pow(process.argv[4], 2));
+}
+
+function leg() {
+    return  Math.sqrt(Math.pow(process.argv[4], 2) - Math.pow(process.argv[3], 2));
+}
+
+if (`${process.argv[2]}`.toLowerCase() == 'hypotenuse') {
+    console.log(hypo());
+} else if (`${process.argv[2]}`.toLowerCase() == 'leg') {
+    console.log(leg());
+} else if (`${process.argv[2]}`.toLowerCase() == 'help') {
+    console.log(
+        'User first input must specify whether you are looking for the hypotenuse or the leg. User second input is one leg of the triangle. If you chose hypotenuse user third input is the second leg of the triangle. If you chose leg user third input is the hypotenuse of the triangle.');
+} else {
+    console.log('Run the program again with \"Help" as your first input.')
+}
--- rpsGame.js
+++ rpsGame.js
@@ -5,9 +5,9 @@ if ((choice.toLowerCase() == 'rock'.toLowerCase()) && (choice2.toLowerCase() == 
     console.log('Player 2 Wins!');
     //Player 1: rock Player 2: paper
 } else if (choice.toLowerCase() != undefined && choice2 == undefined) {
-    //console.log('You need to make 2 inputs! Find a friend!');
+    console.log('You need to make 2 inputs! Find a friend!');
     //Player 1: [has an input] Player 2: [no input]
-    choice2 = Math.random() > .5 ? 'rock' : 'paper';
+    //choice2 = Math.random() > .5 ? 'rock' : 'paper';
     console.log(choice2);
 } else if ((choice.toLowerCase() == 'scissors'.toLowerCase()) && (choice2.toLowerCase() == 'paper'.toLowerCase())) {
     console.log('Player 1 Wins!');
@@ -28,3 +28,14 @@ if ((choice.toLowerCase() == 'rock'.toLowerCase()) && (choice2.toLowerCase() == 
     console.log('Player 1 Wins!');
     //Player 1: rock Player 2: scissors
 }
+
+
+function random() {
+    let number = Math.random();
+    let timesTen = number * 10;
+    let final = Math.floor(timesTen);
+    console.log(final);
+
+}
+
+random();
